06bf3c755fd9931d5dabce9e425bc15f1ea10501
chuyongwei <1429303705@qq.com>
2020-07-14 19:13:40 +0800

    Traversal methods in lesson4-2

--- js/lesson4-2.js
+++ js/lesson4-2.js
@@ -1,1 +1,22 @@
-let grade={}
+let grade = {
+  'lilei': 96,
+  'hanmeimei': 99
+}
+
+// let result = []
+// for (let k in grade) {
+//   result.push(k)
+// }
+
+// console.log(Object.keys(grade).filter(item => item === 'lilei'))
+// console.log(Object.values(grade).filter(item => item > 96))
+
+let result = []
+for (let [k, v] of Object.entries(grade)) {
+  console.log(k, v)
+  if (k === 'lilei') {
+    result.push(k)
+  }
+}
+
+console.log(result)
