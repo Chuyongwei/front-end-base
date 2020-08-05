7c369e1ec3eb26d1ed4b5e71bd517c5b07575dd5
chuyongwei <1429303705@qq.com>
2020-08-05 11:02:54 +0800

    Vue asynchronous operation on ES6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,94 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 v-make-red>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      For a guide and recipes on how to configure / customize this project,
+      <br />check out the
+      <a
+        href="https://cli.vuejs.org"
+        rel="noopener"
+        target="_blank"
+      >vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          rel="noopener"
+          target="_blank"
+        >babel</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          rel="noopener"
+          target="_blank"
+        >eslint</a>
+      </li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li>
+        <a href="https://vuejs.org" rel="noopener" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" rel="noopener" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" rel="noopener" target="_blank">Community Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" rel="noopener" target="_blank">Twitter</a>
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" rel="noopener" target="_blank">News</a>
+      </li>
     </ul>
     <h3>Ecosystem</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li>
+        <a href="https://router.vuejs.org" rel="noopener" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" rel="noopener" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          rel="noopener"
+          target="_blank"
+        >vue-devtools</a>
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" rel="noopener" target="_blank">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" rel="noopener" target="_blank">awesome-vue</a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  async mounted () {
+    const { data } = await axios.get('/user')
+    if (data && data.code === '0') {
+      console.log(data.user)
+    }
+
+    // console.log(axios.get('/user'))
+    // axios.get('/user').then(({ data }) => {
+    //   if (data && data.code === '0') {
+    //     console.log(data.user)
+    //   }
+    // })
   }
 }
 </script>
