04e468fcee84ed41a095329fa40a688c88a611ec
tianyuan <tianyuan@nuc.com>
2020-10-27 21:00:52 +0800

    update 2020.10.27

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,19 +1,24 @@
 <template>
-
-    <view>
-            <view>首页</view>
-    <text class="iconfont iconvideocamera"></text>
-    </view>
+	<view>
+		<view>首页</view>s
+		<text class="iconfont iconvideocamera"></text>
+		<uni-badge text="1"></uni-badge>
+		<uni-badge text="2" type="success" @click="bindClick"></uni-badge>
+		<uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+	</view>
 </template>
-<script lang="ts" src='./index.ts'></script>
-<script>
 
-// import {unibadge} from '@dcloudio/uni-ui'
-// export default {
-//     components: {unibadge}
-// }
+<script>
+	import {
+		uniBadge
+	} from '@dcloudio/uni-ui'
+	//import uniBadge from '@dcloudio/uni-ui/lib/uni-badge/uni-badge.vue' //也可使用此方式引入组件
+	export default {
+		components: {
+			uniBadge
+		}
+	}
 </script>
 
 <style scoped>
-
 </style>
